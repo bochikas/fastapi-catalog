75547bdfd05ac2cb252b5f3a9004eedd68af2ed9
Damir Kambarov <damir.kambarov@qualixdev.com>
2025-04-12 10:55:43 +0500

    main.py file added

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from db.database import engine
+from db.models import Base
+from db.seed_db import seed_db
+
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def drop_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db()
+    await seed_db()
+
+    yield
+    await drop_db()
+
+
+app = FastAPI(lifespan=lifespan)
